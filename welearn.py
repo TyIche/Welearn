dd82b0aa1e844b380119fa458bb96d5047574600
LittlePr1ncess <1123484522@qq.com>
2020-08-19 13:23:26 +0800

    update

--- welearn.py
+++ welearn.py
@@ -12,25 +12,39 @@ def getinfo(url):
 def work(u,x,y,z,html):
     soup = BeautifulSoup(html)
     flag = 0
+    ret  = ""
+    L = {'a','b','c','d'}
     #find 1
     for i in soup.find_all('et-choice'):
-        if(i.parent.name=='et-mobile-only'):
+        #if(i.parent.name=='et-web-only'):
+        #    continue
+        if(i.get('key') not in L):
             continue
         if(flag == 0):
-            print()
+            #print()
+            ret+='\n'
             flag = 1
             print("Unit=%s x=%s y=%s z=%s"%(u,x,y,z))
-        print(i.get('key'))
+            ret+=("Unit=%s x=%s y=%s z=%s"%(u,x,y,z))
+        ret+='\n'+(i.get('key'))
+        #rwz=i.get('ol')
+        #for j in rwz.children:
+        #    print(j.string)
+        #print(i)
     #find 2
+    
     for i in soup.find_all('et-blank'):
         if(i.parent.name=='et-mobile-only'):
             continue
-        if(i.string and flag == 0):
-            print()
+        if(flag == 0):
+            #print()
+            ret+='\n'
             flag = 1
-            print("Unit=%s x=%s y=%s z=%s" %(u,x,y,z))
+            print("Unit=%s x=%s y=%s z=%s"%(u,x,y,z))
+            ret+=("Unit=%s x=%s y=%s z=%s"%(u,x,y,z))
         if(i.string):
-            print(i.string)
+            #print(i.string)
+            ret+='\n'+i.string
     #find 3
     for i in soup.find_all('et-reference'):
         if(i.parent.name=='et-mobile-only'):
@@ -45,36 +59,43 @@ def work(u,x,y,z,html):
                 continue
             for k in j.children:
                 if(flag == 0):
-                    print()
+                    #print()
+                    ret+='\n'
                     flag = 1
-                    print("Unit=%s x=%s y=%s z=%s\n" %(u,x,y,z))
-                print(k.string)   
+                    print("Unit=%s x=%s y=%s z=%s"%(u,x,y,z))
+                    ret+=("Unit=%s x=%s y=%s z=%s"%(u,x,y,z))
+                #print(k.string)   
+                ret+='\n'+k.string
+    return ret
+    
 def main():
-    url1 = 'https://centercourseware.sflep.com/inspire%203/data/'\
+    url1 = 'https://centercourseware.sflep.com/inspire%204/data/'\
     
     #3
-    for u in range(13,18):
+    ans=""
+    for u in range(11,16):
         for x in range(1,7):
             #print(url1+str(u)+'/'+str(x)+'.html')
             html = getinfo(url1+str(u)+'/'+str(x)+'.html')
             #print(html)
             if(html!='0'):
-                work(u,x,0,0,html)
+                ans=ans+work(u,x,0,0,html)
             for y in range(1,5):
                 #print(url1+str(u)+'/'+str(x)+'-'+str(y)+'.html')
                 html = getinfo(url1+str(u)+'/'+str(x)+'-'+str(y)+'.html')
                 #print(html)
                 if(html!='0'):
-                    work(u,x,y,0,html)
+                    ans=ans+work(u,x,y,0,html)
                 for z in range(1,6):
                     #print(url1+str(u)+'/'+str(x)+'-'+str(y)+'-'+str(z)+'.html')
                     html = getinfo(url1+str(u)+'/'+str(x)+'-'+str(y)+'-'+str(z)+'.html')
                     #print(html)
                     if html !='0':
-                        work(u,x,y,z,html)
+                        ans=ans+work(u,x,y,z,html)
                 #url = last
-    #f = open("嘤语答案.txt"，"w")
-    #f.write(ans)
-    #f.close()
+    f = open("out.html","a",encoding='utf-8')  
+    print(ans)
+    f.write(ans)
+    f.close()
 main()
 #zhy goto f
